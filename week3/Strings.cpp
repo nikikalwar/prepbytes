0f0d2de37b580984904709905f1ae8df29615b86
nikikalwar <nikikumar27@gmail.com>
2020-09-07 05:09:58 +0530

    strings in c++

---
+++ week3/Strings.cpp
@@ -0,0 +1,13 @@
+/*
+
+#include<string>
+1.getline()  -> to get the whole sentence
+2.push_back() -->it will add the character to the end of the string
+3.pop_back() -->it will pop the last element.
+
+
+string str2;
+getline(cin,str2);
+cout<<str2;
+
+*/
