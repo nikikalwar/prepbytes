844441b27d93909ff76ed72a6338e7ab29431714
nikikalwar <nikikumar27@gmail.com>
2020-10-22 18:51:04 +0530

    vowel correction in week3

--- week3/NovelVowel.cpp
+++ week3/NovelVowel.cpp
@@ -12,19 +12,23 @@
     while(test--){
    string str2;
    cin>>str2;
-   counter=1;
-   for(i=0;i<str2.length()-1;i=i+1){
-  if(str2[i]=='n'){i=i+1;}
-  else {
-          if(!(str2[i]=='a'||str2[i]=='e'||str2[i]=='i'||str2[i]=='o'||str2[i]=='u'))
-          if((str2[i+1]=='a'||str2[i]=='e'||str2[i]=='i'||str2[i]=='o'||str2[i]=='u'))
-        { counter=0;
+   counter=0;
+   for(i=0;i<str2.length();i=i+1){
+  
+          if(!(str2[i]=='a'||str2[i]=='e'||str2[i]=='i'||str2[i]=='o'||str2[i]=='u'||str2[i]=='n')){
+          if(!(str2[i+1]=='a'||str2[i+1]=='e'||str2[i+1]=='i'||str2[i+1]=='o'||str2[i+1]=='u'))
+        { counter=1;
         }
-          else counter=1;
-  }}
+
+      //  else if(str2[i]==NULL) counter=1;}
+}
+      
+  }
         if(counter==1) cout<<"NO"<<endl;
         else cout<<"YES"<<endl;
           
    }
     return 0;
   }
+
+  
